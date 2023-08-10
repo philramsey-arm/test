586a798d98caeb8e833c251f9d387e855c143fba
Phil Ramsey <phil.ramsey@arm.com>
2023-08-10 11:56:08 +0100

    Add avk.bash
    Change-Id: I85c55833307cda95c5e70d6067b2d188517f5251

---
+++ avk.bash
@@ -0,0 +1,23 @@
+cd dev
+rm -rf dynamorio
+rm -rf dynamorio_build
+
+git clone "https://eu-gerrit-1.euhpc.arm.com/armie/dynamorio"
+
+cd dynamorio
+git checkout upstream-master
+wget https://patch-diff.githubusercontent.com/raw/DynamoRIO/dynamorio/pull/5835.diff
+git apply 5835.diff
+
+cd ..
+mkdir dynamorio_build
+cd dynamorio_build
+
+cmake -DDEBUG=ON -DBUILD_TESTS=ON -DSHOW_RESULTS=ON ../dynamorio
+make -j
+
+cd ..
+
+cd tests/private/avk
+
+./run_avk.py /home/phiram01/dev/dynamorio_build/bin64/drrun sve2_v0.0/build/sve/scapa-00rel0/tests --results results.json
